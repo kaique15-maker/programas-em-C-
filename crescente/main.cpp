eca24ec3585b23de4414f959b688427a611c8786
kaique roberto <kaike.roberto.kaike@gmail.com>
2023-04-06 19:54:55 -0300

    programa em c++ utilizando while

---
+++ crescente/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int x, y, troca;
+
+
+    cout << "Digite dois numeros: "<< endl;
+    cin >> x;
+    cin >> y;
+
+    while (x != y){
+        if (x > y){
+        cout << "DECRESCENTE" << endl;
+    }
+    else {
+        cout << "CRESCENTE" << endl;
+    }
+        cout << "Digite outros dois numeros: " << endl;
+        cin >> x;
+        cin >> y;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
